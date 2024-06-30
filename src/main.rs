6924128de18fdd871950277973499f16377ed180
John Christensen <johnedchristensen@gmail.com>
2024-06-30 16:34:22 -0500

    Use custom buffer struct

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ const WIDTH: usize = 640;
 const HEIGHT: usize = 360;
 
 struct Buffer {
+    //vector of paletteIndicies
     pixels: Vec<u8>,
     width: usize,
     height: usize,
 }
 
+// packedRGB values, indexed by paletteIndex
 const PALETTE: [u32; 4] = [
     (0 << 16) | (0 << 8) | 0,
     (50 << 16) | (50 << 8) | 50,
@@ -18,7 +20,13 @@ const PALETTE: [u32; 4] = [
 ];
 
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut palette_buffer = Buffer {
+        pixels: vec![1; WIDTH * HEIGHT],
+        width: WIDTH,
+        height: HEIGHT,
+    };
+
+    let mut rgb_buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
     let mut window = Window::new(
         "Test - ESC to exit",
@@ -38,16 +46,23 @@ fn main() {
     while window.is_open() && !window.is_key_down(Key::Escape) {
         let elapsed_time = now.elapsed();
         if elapsed_time.as_secs() >= 1 {
-            for i in buffer.iter_mut() {
-                *i = PALETTE[frames % 4];
+            for i in palette_buffer.pixels.iter_mut() {
+                *i = frames % 4;
             }
+
+            rgb_buffer = palette_buffer
+                .pixels
+                .iter()
+                .map(|&palette_index| PALETTE[palette_index as usize])
+                .collect();
+
             now = Instant::now();
             frames += 1;
         }
 
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
         window
-            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .update_with_buffer(&rgb_buffer, WIDTH, HEIGHT)
             .unwrap();
     }
 }
